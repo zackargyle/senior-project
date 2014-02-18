ab7f54d3b9fa8eca64f4c6a94939dc98e3f41e01
Zack Argyle <zargyle@verisage.us>
2014-02-18 12:01:54 -0700

    Updates to api

--- api/views.py
+++ api/views.py
@@ -96,7 +96,7 @@ def getGameData(game):
         teams = Team.objects.filter(game=game)
 
         for team in teams:
-            game_['teams'].append({"name": team.name, "score": team.score})
+            game_['teams'].append({"id": team.id, "name": team.name, "score": team.score})
 
     players = PlayerInstance.objects.filter(game=game)
 
@@ -106,10 +106,8 @@ def getGameData(game):
             team = player.team.name
         game_['players'].append({
             'username': player.player.username,
-            'gun': player.gun.id,
-            'team': team,
-            'num_shots': player.num_shots,
-             'score': player.score if player.score > 0 else 0
+            'team_name': team,
+            'score': player.score
         })
 
     return game_
@@ -138,16 +136,16 @@ class GameList(APIView):
         for game in games:
             games_.append(getGameData(game))
 
-        return Response({'games': games_})
+        return Response(games_)
 
 
-class GameJoin(generics.UpdateAPIView):
+class GameJoin(APIView):
     '''
         lasertag.byu.edu/join/<game_id>
         {
-            "team": null,
+            "team_id": null,
             "username": "psycho",
-            "gun": 4
+            "gun_id": 4
         }
     '''
     def put(self, request, pk, format=None):
@@ -156,13 +154,13 @@ class GameJoin(generics.UpdateAPIView):
 
         team = None
         try:
-            team = Team.objects.get(id=data["team"])
+            team = Team.objects.get(id=data["team_id"])
         except ObjectDoesNotExist:
             pass
 
         gun = None
         try:
-            gun = Gun.objects.get(id=data["gun"])
+            gun = Gun.objects.get(id=data["gun_id"])
         except ObjectDoesNotExist:
             pass
 
@@ -178,7 +176,7 @@ class GameJoin(generics.UpdateAPIView):
 
         team = instance.team.id if instance.team else None
 
-        return Response({'game': game.id, 'mode': game.mode, 'player': instance.id, 'team': team})
+        return Response({'game_id': game.id, 'player_id': instance.id, 'team_id': team})
 
 
 class GameStart(APIView):
@@ -186,79 +184,62 @@ class GameStart(APIView):
         lasertag.byu.edu/start
         {
             "mode": "TEAMS",
-            "players": [{
-                    "gun": 3,
-                    "team": "googlers",
-                    "username": "mac the knife"
-                },{
-                    "gun": 4,
-                    "team": "bingers",
-                    "username": "jack the ripper"
-                }
-            ],
+            "player": {
+                "gun_id": 3,
+                "team_name": "googlers",
+                "username": "mac the knife"
+            },
             "teams": ["googlers", "bingers"],
             "score_limit": null,
-            "time_limit": null,
-            "username": "mac the knife"
+            "time_limit": null
         }
+
     '''
     def post(self, request):
         data = request.DATA
-        players = data["players"]
+        player = data["player"]
         teams = data["teams"]
+        print data
 
         game = Game(mode=data["mode"], state="PLAYING", time_limit=data["time_limit"], score_limit=data["score_limit"])
         game.save()
 
         if data["mode"] == "TEAMS":
-            setupTeamsAndPlayers(teams, players, game)
-        else:
-            setupPlayersOnly(players, game)
+            setupTeams(teams, game)
 
-        player = Player.objects.get(username=data["username"])
-        instance = PlayerInstance.objects.get(game=game,player=player)
+        instance = setupPlayer(player, game)
 
-        team = instance.team.id if instance.team else None
+        team_id = instance.team.id if instance.team else None
 
-        return Response({'game': game.id, 'mode': game.mode, 'player': instance.id, 'team': team})
+        return Response({'game_id': game.id, 'player_id': instance.id, 'team_id': team_id})
 
-def setupTeamsAndPlayers(teams, players, game):
+def setupTeams(teams, game):
     for team_name in teams:
         team = Team(name=team_name, game=game, score=0)
         team.save()
 
-        for p in players:
-            if p["team"] == team_name:
-                player = None
-                try:
-                    player = Player.objects.get(username=p["username"])
-                except ObjectDoesNotExist:
-                    player = Player(username=p["username"])
-                    player.save()
-                gun = Gun.objects.get(pk=p["gun"])
-                instance = PlayerInstance(gun=gun, player=player, team=team, game=game, num_shots=0, score=0)
-                instance.save()
+def setupPlayer(player, game):
+    player_ = None
+    try:
+        player_ = Player.objects.get(username=player["username"])
+    except ObjectDoesNotExist:
+        player_ = Player(username=player["username"])
+        player_.save()
 
-def setupPlayersOnly(players, game):
-    for p in players:
-        player = None
-        try:
-            player = Player.objects.get(username=p["username"])
-        except ObjectDoesNotExist:
-            player = Player(username=p["username"])
-            player.save()
+    gun = Gun.objects.get(pk=player["gun_id"])
 
-        gun = Gun.objects.get(pk=p["gun"])
+    team = Team.objects.get(game=game, name=player["team_name"])
 
-        instance = PlayerInstance(gun=gun, player=player, team=None, game=game, num_shots=0, score=0)
-        instance.save()
+    instance = PlayerInstance(gun=gun, player=player_, team=team, game=game, num_shots=0, score=0)
+    instance.save()
+    return instance
 
 
 class Sync(APIView):
     '''
         lasertag.byu.edu/sync/<game_id>
         {
-            "gun": 3,
+            "player_id": 3,
             "shots_fired": 15,
             "hits_taken": [4,4,4]
         }
@@ -268,7 +249,7 @@ class Sync(APIView):
         data = request.DATA
 
         try:
-            player_instance = PlayerInstance.objects.get(game=game, gun__id=data["gun"])
+            player_instance = PlayerInstance.objects.get(id=data["player_id"])
             player_instance.num_shots = data["shots_fired"]
             player_instance.save()
 
@@ -284,27 +265,16 @@ def getUpdates(game, player):
     team_scores = []
 
     for team in teams:
-        team_members = PlayerInstance.objects.filter(team=team)
-        team_score = 0
-
-        for member in team_members:
-            team_score += member.score
-        
-        score = {'name': team.name, 'score': team_score if (team_score > 0) else 0}
+        score = {'name': team.name, 'score': team.score}
         team_scores.append(score)
 
-        team.score = team_score
-        team.save()
+    players = PlayerInstance.objects.filter(game=game)
+    players_scores = []
 
-    #How many times you've been shot
-    shots = Shot.objects.filter(shooter=player)
+    for player in players:
+        player_scores.append({'username': player.username, 'score': player.score})
 
-    team = player.team
-    team_name = ""
-    if team is not None:
-        team_name = team.name
-
-    return Response({'team_scores': team_scores, 'team': team_name, 'score': player.score if player.score > 0 else 0, 'game_state': game.state, 'times_hit': len(shots)})
+    return Response({'team_scores': team_scores,'player_scores': players_scores, 'score': player.score, 'game_state': game.state})
 
 
 def getPlayerByFrequency(game, frequency):
@@ -328,8 +298,18 @@ def updateShots(game, hits, player_hit):
         shot = Shot(game=game, shooter=player_shooting, target=player_hit)
         shot.save()
 
-        player_shooting.score += 20
+        # Increase shooter's score
+        player_shooting.score += 50
         player_shooting.save()
 
-        player_hit.score -= 10
+        if game.mode == "TEAMS":
+            player_shooting.team.score += 50
+            player_shooting.team.save()
+
+        # Decrease player_hit's score
+        player_hit.score -= 25
         player_hit.save()
+
+        if game.mode == "TEAMS":
+            player_hit.team.score -= 25
+            player_hit.team.save()
